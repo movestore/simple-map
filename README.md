bae1a8585f218c9072b06c1648b75dfd97441693
andreakoelzsch <akoelzsch@ab.mpg.de>
2024-07-03 13:56:11 +0200

    update to move2

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# Static Map
+## Static Map
 MoveApps
 
 Github repository: *github.com/movestore/simple-map*
 
 ## Description
-Simple shiny map with points and lines in the ID colours. Background are coastlines only. 
+Simple shiny map with points and lines in the ID colours. Background are coastlines. 
 
 ## Documentation
 This App gives a simple coastline map with movement tracks. For each animal the locations are added as points that are connected with straight lines. Data of each animal are marked by different colours.
 
 ### Input data
-moveStack in Movebank format
+move2 location object
 
 ### Output data
-moveStack in Movebank format
+move2 location object
 Shiny user interface (UI)
 
 ### Artefacts
@@ -22,7 +22,10 @@ none
 ### Settings 
 `Choose edge size`: The amount of longitude and latitude units that shall be added at the edges of the data's bounding box for better visibility. Default 0.
 
-### Null or error handling:
+### Most common errors
+Please make an issue on Github if you encounter repeated errors.
+
+### Null or error handling
 **Setting `Choose edge size`:** The default value is `0`, which leads to no extra margin of the map around the locations' bounding box.
 
 **Data:** The data are not manipulated, but empty input with no locations (NULL) leads to an error. For calculations in further Apps the input data set is returned.
--- ShinyModule.R
+++ ShinyModule.R
@@ -1,36 +1,40 @@
-library('move')
 library('shiny')
+library('move2')
+library('sf')
 library('maps')
+library('shinycssloaders')
 
 shinyModuleUserInterface <- function(id, label) {
   ns <- NS(id)
   
   tagList(
-    titlePanel("Simple map"),
+    titlePanel("Simple map with coastlines"),
     sliderInput(inputId = ns("num"), 
                 label = "Choose edge size", 
                 value = 0, min = 0, max = 30),
-    plotOutput(ns("map"),height="90vh")
+    withSpinner(plotOutput(ns("map"),height="90vh"))
   )
 }
 
+# The parameter "data" is reserved for the data object passed on from the previous app
 shinyModule <- function(input, output, session, data) {
+  # all IDs of UI functions need to be wrapped in ns()
   dataObj <- reactive({ data })
   current <- reactiveVal(data)
-
-  lon <- reactive({coordinates(data)[,1]})
-  lat <- reactive({coordinates(data)[,2]})
-  ids <- reactive({namesIndiv(data)})
+  
+  lon <- reactive({st_coordinates(data)[,1]})
+  lat <- reactive({st_coordinates(data)[,2]})
+  ids <- reactive({unique(mt_track_id(data))})
   COL <- reactive({rainbow(n=length(ids()))})
-  data_spl <- reactive({move::split(data)})
-
+  data_spl <- reactive({split(data,mt_track_id(data))})
+  
   plotInput <- reactive({
     map("world",xlim=c(min(lon())-input$num,max(lon())+input$num),ylim=c(min(lat())-input$num,max(lat())+input$num))
     
     for (i in seq(along=ids()))
     {
-      loni <- coordinates(data_spl()[[i]])[,1]
-      lati <- coordinates(data_spl()[[i]])[,2]
+      loni <- st_coordinates(data_spl()[[i]])[,1]
+      lati <- st_coordinates(data_spl()[[i]])[,2]
       points(loni,lati,col=COL()[i],pch=20,cex=2)
       lines(loni,lati,col=COL()[i],lwd=1)
     }
